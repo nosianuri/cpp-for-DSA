d3cea0de53dfe86cbd7189a9dfe89466437fe8c7
nosianuri <nurinosia@gmail.com>
2023-11-13 22:02:15 +0600

    add module2

---
+++ module2/increase_size.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int *a = new int[3];
+    int *b = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> a[i];
+        b[i] = a[i];
+    }
+    // for (int i = 0; i < 3; i++)
+    //     cout << b[i] << " ";
+
+    // for (int i = 0; i < 3; i++)
+    //     cout << a[i] << " ";
+    // cout << endl;
+    delete[] a;
+
+    // for (int i = 0; i < 3; i++)
+    //     cout << a[i] << " ";
+
+    a = new int[5];
+    for (int i = 0; i < 3; i++)
+    {
+        a[i] = b[i];
+    }
+    delete[] b;
+    a[3] = 40;
+    a[4] = 50;
+    for (int i = 0; i < 5; i++)
+        cout << a[i] << " ";
+    return 0;
+}
