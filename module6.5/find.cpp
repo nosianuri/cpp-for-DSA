5b9b5586d27e837582a3d41f712f0fadf1904e85
nosianuri <nurinosia@gmail.com>
2023-11-22 21:02:02 +0600

    add module 6.5

---
+++ module6.5/find.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="BRITISHEGYPTGHANA";
+    int t=s.find("EGYPT");
+    // s.replace(t,5,"$");
+    // while (s.find("World") !=-1)
+    // {
+    //     s.replace(s.find("World"),5,"$");
+    // }
+    
+    cout<<t<<endl;
+    return 0;
+}
